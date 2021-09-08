1b22e8367588664e419626ec0113e349521a9401
Dmitry Guzeev <dmitri.guzeev@gmail.com>
2021-09-08 14:47:16 +0300

    Implement basic drag-to-scroll

--- slider.tsx
+++ slider.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useLayoutEffect, useRef, useCallback, useMemo, useState, useEffect } from "react";
 import "./slider.css";
 
 type CNArg = string[] | Record<string, boolean>;
@@ -44,15 +44,15 @@ interface SliderControllerMetaInfo {
 let __globalCRId = 0;
 export const useSliderController = (options: SliderControllerOptions = {}): SliderController => {
   const infinite = options.infinite ?? false;
-  const __controllerId = React.useMemo(() => {
+  const __controllerId = useMemo(() => {
     const res = __globalCRId;
     ++__globalCRId;
     return res;
   }, []);
   const startingSlide = options.startingSlide ?? 0;
-  const [currentSlide, setCurrentSlide] = React.useState<number>(startingSlide);
-  const [metaInfo, setMetaInfo] = React.useState<SliderControllerMetaInfo | null>(null);
-  const prevSlide = React.useCallback(() => {
+  const [currentSlide, setCurrentSlide] = useState<number>(startingSlide);
+  const [metaInfo, setMetaInfo] = useState<SliderControllerMetaInfo | null>(null);
+  const prevSlide = useCallback(() => {
     if (process.env.NODE_ENV === "development") {
       console.assert(metaInfo !== null, "No meta info set");
     }
@@ -65,7 +65,7 @@ export const useSliderController = (options: SliderControllerOptions = {}): Slid
       setCurrentSlide(currentSlide - 1);
     }
   }, [currentSlide, metaInfo]);
-  const nextSlide = React.useCallback(() => {
+  const nextSlide = useCallback(() => {
     if (process.env.NODE_ENV === "development") {
       console.assert(metaInfo !== null, "No meta info set");
     }
@@ -78,7 +78,7 @@ export const useSliderController = (options: SliderControllerOptions = {}): Slid
       setCurrentSlide(currentSlide + 1);
     }
   }, [currentSlide, metaInfo]);
-  const __setMetaInfo = React.useCallback(setMetaInfo);
+  const __setMetaInfo = useCallback(setMetaInfo);
   return { currentSlide, nextSlide, prevSlide, __setMetaInfo, __controllerId };
 };
 
@@ -109,7 +109,6 @@ const Slider = (props: SliderProps) => {
   const { indicator, children, infinite } = props;
   const { __controllerId, __setMetaInfo, nextSlide, prevSlide, currentSlide } =
     props.controller ?? useSliderController(props);
-  console.log("controller #" + __controllerId);
   const classes = props.classes ?? {};
   const numSlides = children.length;
   const moveLeft = () => {
@@ -118,8 +117,8 @@ const Slider = (props: SliderProps) => {
   const moveRight = () => {
     nextSlide();
   };
-  const slidesRef = React.useRef([]);
-  const renderedSlides = React.useMemo(() => {
+  const slidesRef = useRef([]);
+  const renderedSlides = useMemo(() => {
     return children.map((slide, idx) => {
       const active = idx === currentSlide;
       slidesRef.current[idx] = React.createRef(null);
@@ -134,18 +133,15 @@ const Slider = (props: SliderProps) => {
       );
     });
   }, [children, currentSlide]);
-  const sliderContainerRef = React.useRef(null);
+  const sliderContainerRef = useRef(null);
 
-  React.useLayoutEffect(() => {
-    console.log(slidesRef);
+  useLayoutEffect(() => {
     if (slidesRef.current.length === 0) return;
     const elem = slidesRef.current[currentSlide].current;
-    console.log(elem);
     sliderContainerRef.current.scrollLeft = elem.offsetLeft;
-    console.log("set: " + sliderContainerRef.current.scrollLeft + " because of " + elem.offsetLeft);
   }, [renderedSlides]);
 
-  const renderedIndicator = React.useMemo(() => {
+  const renderedIndicator = useMemo(() => {
     if (indicator === undefined) return null;
     const containerCn = itcn[indicator];
     if (process.env.NODE_ENV === "development") {
@@ -173,13 +169,50 @@ const Slider = (props: SliderProps) => {
     return <div className={cn(["rslider-indicator", containerCn])}>{renderedLines}</div>;
   }, [currentSlide]);
 
-  React.useEffect(() => {
+  useEffect(() => {
     __setMetaInfo({ numSlides: children.length });
   }, [children.length, __controllerId]);
 
   const amountOfSlides = children.length;
   const isThereSomethingToLeft = infinite || currentSlide !== 0;
   const isThereSomethingToRight = infinite || currentSlide !== (amountOfSlides - 1);
+  const [mouseDown, setMouseDown] = useState<bool>(false);
+
+  const onMouseDown = useCallback((e) => {
+    const el = sliderContainerRef.current!;
+    initialMouseScrollPos.current = { x: e.clientX, y: e.clientY, left: el.scrollLeft, top: el.scrollTop };
+    setMouseDown(true);
+  }, []);
+
+  const onMouseUp = useCallback((e) => {
+    setMouseDown(false);
+    document.removeEventListener("mousemove", onMouseScrollMove);
+    document.removeEventListener("mouseup", onMouseUp);
+  }, []);
+  const initialMouseScrollPos = useRef({ left: 0, top: 0, x: 0, y: 0 });
+
+  const onMouseScrollMove = useCallback((e) => {
+    const dx = e.clientX - initialMouseScrollPos.current.x;
+    const dy = e.clientY - initialMouseScrollPos.current.y;
+    if (dx < 0) {
+      setMouseDown(false);
+      nextSlide();
+    } else if (dx > 0) {
+      setMouseDown(false);
+      prevSlide();
+    }
+  }, [nextSlide, prevSlide]);
+
+  useEffect(() => {
+    if (mouseDown) {
+      document.addEventListener("mousemove", onMouseScrollMove);
+      document.addEventListener("mouseup", onMouseUp);
+    }
+    return () => {
+      document.removeEventListener("mousemove", onMouseScrollMove);
+      document.removeEventListener("mouseup", onMouseUp);
+    };
+  }, [mouseDown, onMouseScrollMove]);
 
   return (
     <div className={cn([props.className, "rslider"])}>
@@ -192,7 +225,7 @@ const Slider = (props: SliderProps) => {
       </div>
       <div className={cn(["rslider__current"])}>
         <div className={"rslider__view"}>
-          <div className={"rslider__content"} ref={sliderContainerRef}>
+          <div className={"rslider__content"} ref={sliderContainerRef} onMouseDown={onMouseDown}>
             {renderedSlides}
           </div>
         </div>
